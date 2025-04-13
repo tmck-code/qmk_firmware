fe43208d629d9eb648b48d10a1d17ab87d89b0db
Tom McKeesick <tmck01@gmail.com>
2025-04-14 00:36:35 +1000

    updating with export from ergodox ez tool

---
+++ keyboards/ergodox_ez/keymaps/tmck-code/zsa_ergodox_ez_m32u4_base_tmck-code-coding_source/config.h
@@ -0,0 +1,15 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+
+#undef DEBOUNCE
+#define DEBOUNCE 28
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 170
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define SERIAL_NUMBER "AzGo6/PndRJ"
+#define LAYER_STATE_8BIT
+
